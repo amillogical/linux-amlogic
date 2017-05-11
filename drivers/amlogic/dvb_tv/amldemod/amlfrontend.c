7e86abfa5724fd207f411203c857cc5b1666e54b
Zhenwu Che <zhenwu.che@amlogic.com>
2017-05-11 16:47:04 +0800

    PD#143822: dvb: fix atsc suspend/resume no signal issue
    Change-Id: Ifd0fb1109de45715717a1f44468ba2f41aafb003
    Signed-off-by: Zhenwu Che <zhenwu.che@amlogic.com>

--- drivers/amlogic/dvb_tv/amldemod/amlfrontend.c
+++ drivers/amlogic/dvb_tv/amldemod/amlfrontend.c
@@ -1208,6 +1208,7 @@ static int gxtv_demod_fe_resume(struct aml_fe_dev *dev)
 {
 	int memstart_dtmb;
 	pr_inf("gxtv_demod_fe_resume\n");
+	vdac_enable(1, 0x2);
 /*	demod_power_switch(PWR_ON);*/
 	Gxtv_Demod_Dtmb_Init(dev);
 	memstart_dtmb = dev->fe->dtv_demod->mem_start;
@@ -1222,6 +1223,7 @@ static int gxtv_demod_fe_suspend(struct aml_fe_dev *dev)
 {
 	pr_inf("gxtv_demod_fe_suspend\n");
 /*	demod_power_switch(PWR_OFF);*/
+	vdac_enable(0, 0x2);
 	return 0;
 }
 
